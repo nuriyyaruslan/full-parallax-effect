1ef65efc1f7886c15d56e918023528ece4f0b5fe
nuriyyaruslan <nuriyya.isgandarova@gmail.com>
2018-06-26 18:54:55 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# full-parallax-effect
---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    wow = new WOW(
+        {
+            boxClass:     'wow',      // default
+            animateClass: 'animated', // default
+            mobile:       false,       // default
+            live:         true        // default
+        }
+    )
+    wow.init();
+})
+
+
+
